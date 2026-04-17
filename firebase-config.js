f3891d190d4d5c3bb08f5188ec7f7e7aa5edc4b4
ctv767825-gif <ctv767825@gmail.com>
2026-04-17 13:34:48 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,14 @@
-window.firebaseConfig = {
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
   apiKey: "AIzaSyAkFnUnVgcc8WzougbBjC7x_PXrb0xKBTA",
   authDomain: "devindra-mart.firebaseapp.com",
   projectId: "devindra-mart",
   storageBucket: "devindra-mart.firebasestorage.app",
   messagingSenderId: "394816688594",
-  appId: "1:394816688594:web:77577dbcade5f19942b80b",
-  measurementId: "G-1RL8KGF507"
+  appId: "1:394816688594:web:77577dbcade5f19942b80b"
 };
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
